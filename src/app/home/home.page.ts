8c531b3681f567700eca5feedcaaca293ddcf3cb
Abdessadiq <abdoubhm2018@gmail.com>
2023-05-19 17:16:40 +0100

    ÃAjouter l ensemble des pages de l application

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public contact={
+    name: "FST SETTAT CIGMA",
+    tel: "0547879292",
+    mail:"scoole@cigma.org",
+    location:"/assets/images/loc.png",
+    logo:"/assets/images/logo.png",
+
+  }
 
   constructor() {}
 
