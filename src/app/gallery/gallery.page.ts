78245d9b2f972851b52c9bdc1199f5f3cad526dc
Abdessadiq <abdoubhm2018@gmail.com>
2023-05-21 23:54:36 +0100

    modifier sur l'affichage de la page des images ..

--- src/app/gallery/gallery.page.ts
+++ src/app/gallery/gallery.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.page.scss'],
 })
 export class GalleryPage implements OnInit {
-
-  constructor() { }
+  public keyword:any;
+  public currenPage:number=1;
+  public size:number=13;
+  public dataImages: any;
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit() {
   }
 
+  onLoadImages() {
+  this.httpClient.get("https://pixabay.com/api/?key=36624956-e2a9b46e9b0921cb5a52d3f39&q="
+    +this.keyword+"&per_page="+this.size+"&page="+this.currenPage)
+    .subscribe(data=>{
+      this.dataImages=data;
+    }, err=>{
+      console.log(err);
+    })
+  }
 }
