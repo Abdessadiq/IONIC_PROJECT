2fa40831ad75524a0d036d81c18bd7f849b4a117
Abdessadiq <abdoubhm2018@gmail.com>
2023-05-21 16:52:57 +0100

    Ajouter le traitement pour consomer l API de weather Puis Ajouter la patie view Pour afficher Data ..

--- src/app/meteo/meteo.page.ts
+++ src/app/meteo/meteo.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-meteo',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meteo.page.scss'],
 })
 export class MeteoPage implements OnInit {
+  public city:any;
+  public dataMeteo:any;
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit() {
   }
 
+  onLoadMeteo() {
+    this.httpClient.get("https://api.openweathermap.org/data/2.5/forecast?q="+this.city+"&appid=88f101582b11edeaa502a4f0fe10c1f1")
+      .subscribe(data=>
+      {
+        this.dataMeteo=data;
+      }, err=>{
+        console.log(err)
+      })
+
+  }
 }
