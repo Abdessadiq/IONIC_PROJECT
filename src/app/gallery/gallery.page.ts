a939672ad9ba80cd7da6283573962dbc216ffc1e
Abdessadiq <abdoubhm2018@gmail.com>
2023-05-24 00:32:17 +0100

    Ajouter la fonctionnalitÃe de l infity scrolling ..

--- src/app/gallery/gallery.page.ts
+++ src/app/gallery/gallery.page.ts
@@ -9,20 +9,55 @@ import {HttpClient} from "@angular/common/http";
 export class GalleryPage implements OnInit {
   public keyword:any;
   public currenPage:number=1;
-  public size:number=13;
-  public dataImages: any;
+  public size:number=10;
+  public dataImages: any[] =[];
+  public totalPages: number=1;
   constructor(private httpClient:HttpClient) { }
 
   ngOnInit() {
   }
 
   onLoadImages() {
-  this.httpClient.get("https://pixabay.com/api/?key=36624956-e2a9b46e9b0921cb5a52d3f39&q="
-    +this.keyword+"&per_page="+this.size+"&page="+this.currenPage)
-    .subscribe(data=>{
-      this.dataImages=data;
-    }, err=>{
-      console.log(err);
-    })
+    this.dataImages=[];
+    this.currenPage=1;
+    this.totalPages=0;
+    this.doSearch();
+
   }
+  private doSearch() {
+    this.httpClient.get<PixabayResponse>("https://pixabay.com/api/?key=36624956-e2a9b46e9b0921cb5a52d3f39&q="
+      +this.keyword+"&per_page="+this.size+"&page="+this.currenPage)
+      .subscribe(data => {
+          data.hits.forEach(pic => {
+            this.dataImages.push(pic);
+            this.totalPages=data.totalHits/this.size;
+          });
+        },
+        err => {
+          console.log(err);
+        })
+
+  }
+
+  loadData(event: any) {
+    console.log(this.currenPage+"/"+this.totalPages);
+    if (this.currenPage < this.totalPages){
+      ++this.currenPage;
+      this.doSearch();
+      event.target.complete();
+    }
+   // if (this.currenPage>=this.totalPages)
+     // event.target.disabled=true;
+
+
+  }
+
+
 }
+
+
+interface PixabayResponse {
+  hits: any[]; // Remplacez 'any' par le type approprié pour les objets 'pic'
+  totalHits:number;
+}
+
